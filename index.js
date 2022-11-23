7d7dc2e0212b6c7c9a6a296cf28348930c87ad7c
João Priante <jm.priante@gmail.com>
2022-11-23 12:36:27 -0300

    estruturação back-end

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const { Random } = require("random-js");
+const { Random } =  require("random-js");
 
-const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const lower = 'abcdefghijklmnopqrstuvwxyz'
-const number = '0123456789'
-const symbols = '!@#$%&*()?°/|'
+const random = new Random();
 
-const random = new Random(
-    MersenneTwister19937.seedWithArray([0x12345678, 0x90abcdef])
-  );
-console.log(Random())
+let password;
+const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const lower =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+const number = ['0','1','2','3','4','5','6','7','8','9']
+const symbols = ['!','@','#','$','%','&','*','(',')','?','°','/','|',`'`,`"`,'=','+','-','_']
+
+let i = random.integer(0,4)
+
+if(i === 0){
+
+}
