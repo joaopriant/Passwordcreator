eb0fe1553b3a37f8fd475b78a39e0314841cb2d7
João Priante <jm.priante@gmail.com>
2022-11-23 12:16:52 -0300

    install do random

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const { Random } = require("random-js");
 
 const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const lower = 'abcdefghijklmnopqrstuvwxyz'
 const number = '0123456789'
 const symbols = '!@#$%&*()?°/|'
 
-console.log(rando(5))
+const random = new Random(
+    MersenneTwister19937.seedWithArray([0x12345678, 0x90abcdef])
+  );
+console.log(Random())
