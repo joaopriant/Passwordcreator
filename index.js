af2fdfc64316c0437f32f5e79a3587eb8b34be71
João Priante <jm.priante@gmail.com>
2022-11-23 22:10:21 -0300

    js atualizado

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { Random } =  require("random-js");
 
 const random = new Random();
 
-let password = []
+function gerador(){
+let password = [];
 const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const lower =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 const number = [0,1,2,3,4,5,6,7,8,9]
@@ -12,24 +13,38 @@ const c = 15
 while (c > password.length){
 
     let i = random.integer(0,3)
-    console.log(i)
+
     if(i === 0){
         i = random.integer(0,25)
-        password.push(upper[i])
-        console.log(upper[i])
-    }else if( i === 1){
+        password = password+upper[i]
+    }
+    else if( i === 1){
         i = random.integer(0,25)
-        password.push(lower[i])
-        console.log(lower[i])
-    }else if(i === 2){
+        password = password+lower[i] 
+    }
+    else if(i === 2){
         i = random.integer(0,9)
-        password.push(number[i])
-        console.log(number[i])
-    }else if(i === 3){
+        password = password+number[i]
+    }
+    else if(i === 3){
         i = random.integer(0,24)
-        password.push(symbols[i])
-        console.log(i)
-        console.log(symbols[i])
+        password = password+symbols[i]
+    }else{
+        continue
     }
 }
 console.log(password)
+}
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+  });
+  
+  document.getElementById('execCopy').addEventListener('click', execCopy);
+  function execCopy() {
+    document.querySelector("#input").select();
+    document.execCommand("copy");
+  }
+
+
+gerador()
